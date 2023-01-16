bc9cea2bd7e8c5d22867f0a67d03a9c8881e0088
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-16 07:28:48 -0300

    criando modal

---
+++ src/componentes/ModalProjeto1/index.js
@@ -0,0 +1,28 @@
+import './modal.css'
+import projeto1 from '../../imagens/projeto1-1.png'
+
+export default function Modal (props) { 
+    
+    const { className, funcaoEsconder } = props
+
+    return(
+        <div className={`${className} modal`}>
+            <div className='modal-header'>
+                <h3 className='modal-titulo'>Projeto construindo o futuro</h3>
+                <button className='modal-doacao'>DOE AGORA</button>
+            </div>
+
+            <div className='modal-imagem'>
+                <img src={projeto1} alt='crinças brincando'></img>
+            </div>
+
+            <div className='modal-content'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus sed orci quis facilisis. Donec pulvinar eros a risus iaculis, eget dapibus diam rutrum. Morbi feugiat turpis et elit mollis, ut maximus urna hendrerit. Aenean quis sodales felis. Quisque quam purus, sodales eget rhoncus nec, porta vel diam. Nulla ultricies, velit at convallis ornare, diam tellus iaculis metus, non accumsan est est id nisl. Pellentesque eget dui vitae erat mattis elementum. Morbi et neque nisl.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus sed orci quis facilisis. Donec pulvinar eros a risus iaculis, eget dapibus diam rutrum. Morbi feugiat turpis et elit mollis, ut maximus urna hendrerit. Aenean quis sodales felis. Quisque quam purus, sodales eget rhoncus nec, porta vel diam. Nulla ultricies, velit at convallis ornare, diam tellus iaculis metus, non accumsan est est id nisl. Pellentesque eget dui vitae erat mattis elementum. Morbi et neque nisl.</p>
+            </div>
+
+            <div className='botao-fechar' onClick={funcaoEsconder}></div>
+        </div>
+    )
+}
